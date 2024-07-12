963813844778811e797264e7debc3047f665bc2b
paivaNick <nipaiva93@gmail.com>
2024-07-12 14:12:11 -0300

    config: adding first test route to project

---
+++ src/main/routes/trips_routes.py
@@ -0,0 +1,10 @@
+from flask import jsonify, Blueprint
+
+
+trips_routes_bp = Blueprint("trip_routes", __name__)
+
+@trips_routes_bp.route("/trips", methods=["POST"])
+def create_trips():
+    return jsonify({"ola": "mundo"}), 200
+
+
